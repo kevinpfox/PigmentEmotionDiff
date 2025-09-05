899b6e21736f5ea2f1ba36463facef19c5b33f79
Kevin Fox <kevin.fox@babycenter.com>
2025-09-05 07:19:45 -0700

    Emotion vs Pigment CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import { withPigment } from '@pigment-css/nextjs-plugin';
-import { createTheme } from '@mui/material';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPigment(nextConfig, {
-  theme: createTheme({
-    cssVariables: {
-      colorSchemeSelector: 'class',
-    },
-    colorSchemes: { light: true, dark: true },
-    typography: {
-      fontFamily: 'var(--font-roboto)',
-    },
-  }),
-  transformLibraries: ['@mui/material'],
-});
+export default nextConfig;
